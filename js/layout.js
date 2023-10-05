f1d256652891e303b315cea560dac829a68e570f
Zeno (Cheng-Chuan) Peng <fgis2gi@gmail.com>
2023-10-05 01:33:13 -0700

    Add airflow

--- js/layout.js
+++ js/layout.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let skillsets = {
+    populateSkills();
+
+});
+
+
+function populateSkills() {
+        let skillsets = {
         "Splunk": ["Go", "OpenTelemetry", "Kubernetes", "GitHub Actions"],
         "Advantech": ["C#", ".NET MVC", "Azure Pipelines", "Azure Blob", "Azure Cosmos DB"],
         "Titansoft": ["C#", ".NET CORE", "Redis", "GRPC", "GCP Spanner", "GCP Pub/Sub"],
@@ -16,4 +22,24 @@ document.addEventListener("DOMContentLoaded", function() {
         let skillText = "<strong>Skillset: </strong> " + skills.join(", "); 
         jobPara.innerHTML += skillText; // Append the skills to the existing job text using innerHTML since we're adding HTML content
     });
-});
+}
+
+
+let topButton = document.getElementById("topBtn");
+
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
