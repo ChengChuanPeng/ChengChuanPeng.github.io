cfd354afc501538dc740a7b673d6e94577fc5498
Zeno (Cheng-Chuan) Peng <fgis2gi@gmail.com>
2023-10-16 01:27:20 -0700

    Add Project

--- js/layout.js
+++ js/layout.js
@@ -1,37 +1,49 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    // Call the populateSkills function when the DOM is fully loaded
     populateSkills();
-
 });
 
-
 function populateSkills() {
-        let skillsets = {
+    // Define a dictionary of skills for different companies
+    let skillsets = {
         "Splunk": ["Go", "OpenTelemetry", "Kubernetes", "GitHub Actions"],
         "Advantech": ["C#", ".NET MVC", "Azure Pipelines", "Azure Blob", "Azure Cosmos DB"],
         "Titansoft": ["C#", ".NET CORE", "Redis", "GRPC", "GCP Spanner", "GCP Pub/Sub"],
         "Micron": ["SQL", "Snowflake", "Python", "Flask", "Airflow", "Shell Script", "NiFi", "Splunk", "Docker", "Openshift", "Hadoop", "Hive", "Hbase"]
     };
 
+    // Select all elements with the class "job"
     let jobs = document.querySelectorAll(".job");
-    
+
+    // Iterate through each job element
     jobs.forEach(job => {
+        // Get the company name from the data-company attribute
         let company = job.getAttribute("data-company");
+
+        // Get the skills associated with the company from the skillsets dictionary
         let skills = skillsets[company];
 
+        // Find the paragraph element within the job element
         let jobPara = job.querySelector("p");
-        let skillText = "<strong>Skillset: </strong> " + skills.join(", "); 
-        jobPara.innerHTML += skillText; // Append the skills to the existing job text using innerHTML since we're adding HTML content
+
+        // Create a string to display the skills
+        let skillText = "<strong>Skillset: </strong> " + skills.join(", ");
+
+        // Append the skills string to the existing job text using innerHTML
+        jobPara.innerHTML += skillText;
     });
 }
 
-
+// Get a reference to the "topBtn" element
 let topButton = document.getElementById("topBtn");
 
-window.onscroll = function() {
+// Add a scroll event listener
+window.onscroll = function () {
     scrollFunction();
 };
 
 function scrollFunction() {
+    // Show or hide the "topBtn" based on scroll position
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         topButton.style.display = "block";
     } else {
@@ -39,7 +51,8 @@ function scrollFunction() {
     }
 }
 
+// Scroll to the top of the page when the "topBtn" is clicked
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 }
